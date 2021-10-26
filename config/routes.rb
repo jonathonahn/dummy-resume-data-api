1a236ef9b8bd10a6ce31e9980f452af6836e40f3
jonathon <ahnjon77@gmail.com>
2021-10-26 09:31:52 -0700

    adds student controller and associated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  "/students" => "students#index"
+  "/students" => "students#show"
 end
